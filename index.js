b4724c17f2595544ba4061360c02bf1d2c0b81e3
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-04-14 22:50:56 +0300

    feat: RM-7 add prettier deps

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type("html").send("Hello world"));
+app.get('/', (req, res) => res.type('html').send('Hello world'));
 
 const server = app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
