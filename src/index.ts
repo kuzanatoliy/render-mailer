389a3f946f693733120865a3ef9f2049172adb8c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-04-15 22:18:42 +0300

    feat: RM-3 add nodemailer

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
+import { createTransport } from 'nodemailer';
+
+import {
+  MAILER_HOST,
+  MAILER_PORT,
+  MAILER_USER,
+  MAILER_PASS,
+  MAILER_TO,
+} from './constants';
+
+const transporter = createTransport({
+  host: MAILER_HOST,
+  port: MAILER_PORT,
+  secure: false,
+  auth: {
+    user: MAILER_USER,
+    pass: MAILER_PASS,
+  },
+});
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req: any, res: any) => res.type('html').send('Hello world'));
+app.get('/', async (req: Request, res: Response) => {
+  const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
+    to: MAILER_TO,
+    subject: 'Hello ✔',
+    text: 'Hello world?',
+  });
+  res.type('html').send(info.messageId);
+});
 
 const server = app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
